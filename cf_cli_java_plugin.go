f460dbf7c17f6266e386070f83920a7199018dba
GitHub Action <action@github.com>
2026-01-14 15:58:16 +0000

    Set version to v4.0.2

--- cf_cli_java_plugin.go
+++ cf_cli_java_plugin.go
@@ -968,7 +968,7 @@ func (c *JavaPlugin) GetMetadata() plugin.PluginMetadata {
 		MinCliVersion: plugin.VersionType{
 			Major: 4,
 			Minor: 0,
-			Build: 0,
+			Build: 2,
 		},
 		Commands: []plugin.Command{
 			{
